06d69d986817e29d7505a64e7784c9cf0e6662d8
NinjaKnight1 <christoffer.dam.hansen@gmail.com>
2023-06-09 10:32:15 +0200

    Update Weather.dart

--- life_hub/lib/APIs/Weather.dart
+++ life_hub/lib/APIs/Weather.dart
@@ -1,22 +1,44 @@
 import "dart:convert";
 import 'package:http/http.dart' as http;
 
-String key = '486495597ff0499394a202301230606';
-String location = '55.673185, 12.563980';
+String key = '486495597ff0499394a202301230606';  // API key
+// TODO: Change location to user location
+String location = '55.673185, 12.563980'; // location 
 String URL = 'https://api.weatherapi.com/v1/current.json?q=$location&key=$key';
 
-class weather {
-  final String? location;
-  final double? temperature;
-  final double? temperatureFeel;
-  final String? condition;
-  final int? conditionCode;
-  final double? windSpeed;
-  final double? windDegree;
-  final double? precip;
-  final int? uv;
+Future<Weather> fetchWeather() async {
+  final response = await http.get(Uri.parse(URL));
+  print(response.statusCode);
+  if (response.statusCode == 200) {
+    return Weather.fromJson(jsonDecode(response.body));
+  } else {
+    throw Exception('Failed to create');
+  }
+}
 
-  const weather(
+main() async {
+  var weathernow = await fetchWeather();
+  // ignore: avoid_print
+  print(weathernow.location +
+      " " +
+      weathernow.temperature.toString() +
+      " C°, " +
+      weathernow.condition);
+}
+
+
+class Weather {
+  final String location;
+  final double temperature;
+  final double temperatureFeel;
+  final String condition;
+  final int conditionCode;
+  final double windSpeed;
+  final int windDegree;
+  final double precip;
+  final double uv;
+
+  const Weather(
       {required this.location,
       required this.temperature,
       required this.temperatureFeel,
@@ -27,8 +49,8 @@ class weather {
       required this.precip,
       required this.uv});
 
-  factory weather.fromJson(Map<String, dynamic> json) {
-    return weather(
+  factory Weather.fromJson(Map<String, dynamic> json) {
+    return Weather(
       location: json['location']['name'],
       temperature: json['current']['temp_c'],
       temperatureFeel: json['current']['feelslike_c'],
