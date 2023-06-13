ca2ab33825a2b726b2f7b338e5cfbdc9d4f49d00
DeMoManique <frederik.hvarregaard.andersen@gmail.com>
2023-06-13 09:54:22 +0200

    a Simple shopping list

--- life_hub/lib/Widgets/widgetShopList.dart
+++ life_hub/lib/Widgets/widgetShopList.dart
@@ -6,5 +6,32 @@ Widget ShopWidget(BuildContext context) {
   return Square(
     context,
     color: Colors.yellow[700],
+    child: ListView(
+      children: StringListToTextList(getShoppingList()),
+    ),
   );
+}
+
+List<Widget> StringListToTextList(List<String>? ShoppingList) {
+  List<Widget> result = [];
+  for (String element in ShoppingList!) {
+    result.add(Text(element));
+  }
+  return result;
+}
+
+List<String> getShoppingList() {
+  return [
+    'mel',
+    'æg',
+    'vanilje',
+    'pølser',
+    'gulerødder',
+    'Christopher',
+    'hvide drenge',
+    'brød',
+    'olie',
+    'mælk',
+    'en hel gris',
+  ];
 }
