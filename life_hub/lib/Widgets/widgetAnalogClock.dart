c5513dc227b9969557d250436d84a254f4b9485b
NinjaKnight1 <christoffer.dam.hansen@gmail.com>
2023-06-14 16:52:42 +0200

    Analog Clock finished

--- life_hub/lib/Widgets/widgetAnalogClock.dart
+++ life_hub/lib/Widgets/widgetAnalogClock.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:life_hub/Widgets/AnalogClockArm.dart';
 import 'package:life_hub/Widgets/widgetShapes.dart';
 import 'package:life_hub/Widgets/widgetSetup.dart';
 
@@ -8,12 +9,17 @@ DateTime now = DateTime.now();
 
 @override
 Widget analogClockWidget(BuildContext context) {
+  List<Widget> clocklines = [];
+  for (var i = 0; i < 13; i++) {
+    clocklines.add(analogClockNumbersWidget(context, i));
+  }
   return StreamBuilder(
       stream: Stream.periodic(const Duration(seconds: 1)),
       builder: ((context, snapshot) {
         now = DateTime.now();
         String minute = now.minute.toString();
         String second = now.second.toString();
+        double hourlength = getWidth(context) * 0.06;
         if (second.length == 1) {
           // make so it is 12.00 instead of 12.0
           second = '0' + second;
@@ -35,12 +41,30 @@ Widget analogClockWidget(BuildContext context) {
                 decoration: BoxDecoration(
                     shape: BoxShape.circle,
                     color: Color.fromARGB(255, 240, 251, 255)),
-                    child: RotatedBox(
-                      quarterTurns: 3,
-                      
+                child: Stack(
+                  children: [
+                    Stack(
+                      children: clocklines,
                     ),
+                    // Hour hand
+                    analogClockArmWidget(context, now, 1),
+                    // Minute hand
+                    analogClockArmWidget(context, now, 2),
+                    // Second hand
+                    analogClockArmWidget(context, now, 3),
+                    // Center dot
+                    Center(
+                      child: Container(
+                        height: getWidth(context) * 0.015,
+                        width: getWidth(context) * 0.015,
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Color.fromARGB(255, 0, 0, 0)),
+                      ),
+                    ),
+                  ],
+                ),
               )),
-              
         );
       }));
 }
